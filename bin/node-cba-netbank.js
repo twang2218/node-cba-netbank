f51498ff1e28ae67281b23b0eee6062c293eec42
Tao Wang <twang2218@gmail.com>
2016-01-09 02:03:25 +1100

    * Fix CLI tool for `jshint`.

--- bin/node-cba-netbank.js
+++ bin/node-cba-netbank.js
@@ -28,6 +28,8 @@ var questionTryAgain = {
 	message: 'Failed to logged in, try again?'
 };
 
+var accounts = [];
+
 function getAccountTitle(account) {
 	var title = account.nickname + ' (' + account.bsbNumber + ' ' + account.number +
 		') ' + '\t=>\t$' + account.balance;
@@ -40,71 +42,80 @@ function getTransactionTitle(transaction) {
 	return title;
 }
 
-function login() {
-	//  user input credential
-	inquirer.prompt([questionUsername, questionPassword], function (answer) {
-		//  login
-		netbank.login(answer, function (err, accounts) {
-			if (err === null && accounts.length > 0) {
-				var questionSelectAccount = {
-					type: 'list',
-					name: 'account',
-					message: 'Which account you want to download the transactions history?',
-					choices: []
-				};
+function credentialHandler(answer) {
+	//  login
+	netbank.login(answer, function (err, accountList) {
+		if (err === null && accountList.length > 0) {
+			accounts = accountList;
+			var questionSelectAccount = {
+				type: 'list',
+				name: 'account',
+				message: 'Which account?',
+				choices: []
+			};
 
-				for (var index in accounts) {
-					var account = accounts[index];
-					questionSelectAccount.choices.push(getAccountTitle(account));
-				}
-				questionSelectAccount.choices.push('<Quit>');
-
-				//  user select an account to download transaction, or quit.
-				inquirer.prompt(questionSelectAccount, function (answer) {
-					for (var index in accounts) {
-						var account = accounts[index];
-						if (answer.account === getAccountTitle(account)) {
-							//  download transactions
-							console.log('Downloading transactions history ...');
-							netbank.getTransactions(account, function (err, transactions) {
-								if (err === null && transactions.length > 0) {
-									console.log('Retrieved ' + transactions.length +
-										' transactions.');
-
-									var questionTransactions = {
-										type: 'list',
-										name: 'account',
-										message: 'This is the retrieved transactions:',
-										choices: []
-									};
-
-									for (var index in transactions) {
-										var transaction = transactions[index];
-										questionTransactions.choices.push(getTransactionTitle(
-											transaction));
-									}
-
-									inquirer.prompt(questionTransactions, function (answer) {
-										//  bye
-										console.log('bye');
-									});
-								} else {
-									console.error(err);
-								}
-							});
-						}
-					}
-				});
-			} else {
-				inquirer.prompt(questionTryAgain, function (answer) {
-					if (answer.tryagain) {
-						console.log();
-						login();
-					}
-				});
+			for (var index in accounts) {
+				var account = accounts[index];
+				questionSelectAccount.choices.push(getAccountTitle(account));
 			}
-		});
+			questionSelectAccount.choices.push('<Quit>');
+
+			//  user select an account to download transaction, or quit.
+			inquirer.prompt(questionSelectAccount, accountSelectedHandler);
+		} else {
+			inquirer.prompt(questionTryAgain, function (answer) {
+				if (answer.tryagain) {
+					console.log();
+					main();
+				}
+			});
+		}
 	});
 }
 
-login();
+function accountSelectedHandler(answer) {
+	for (var index in accounts) {
+		var account = accounts[index];
+		if (answer.account === getAccountTitle(account)) {
+			//  download transactions
+			console.log('Downloading transactions history ...');
+			netbank.getTransactions(account, transactionReceivedHandler);
+		}
+	}
+}
+
+function transactionReceivedHandler(err, transactions) {
+	if (err === null && transactions.length > 0) {
+		console.log('Retrieved ' + transactions.length +
+			' transactions.');
+
+		var questionTransactions = {
+			type: 'list',
+			name: 'account',
+			message: 'Transactions:',
+			choices: []
+		};
+
+		for (var index in transactions) {
+			var transaction = transactions[index];
+			questionTransactions.choices.push(getTransactionTitle(
+				transaction));
+		}
+
+		inquirer.prompt(questionTransactions, quit);
+	} else {
+		console.error(err);
+	}
+}
+
+function quit() {
+	//  bye
+	console.log('bye');
+}
+
+function main() {
+	//  user input credential
+	inquirer.prompt([questionUsername, questionPassword], credentialHandler);
+}
+
+main();
