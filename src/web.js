b0017dea53d2cbf459dfdb44e5726e51d796febb
Tao Wang <twang2218@gmail.com>
2017-07-03 11:46:09 +1000

    Add 'no-cache' to the HTTP request headers to avoid caching issue
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- src/web.js
+++ src/web.js
@@ -10,6 +10,7 @@ const DEFAULT_OPTION = {
   resolveWithFullResponse: true,
   headers: {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Cache-Control': 'no-cache',
   },
 };
 
